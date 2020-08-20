d08e5b4423a10549216dbf166076f9cd4db0858c
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-08-20 17:30:37 +0200

    section main design finish, update

--- src/js/main.js
+++ src/js/main.js
@@ -2,26 +2,49 @@
 
 // Variables Globales
 
-let dicoMots = ["tennis", "tennis", "tennis", "tennis", "tennis", "tennis", "tennis"];
+let dicoMots = ["sorcière", "tennis", "tennis", "tennis", "tennis", "tennis", "tennis"];
 
 let motRandom = generateRandomWord().toUpperCase();
-console.log(motRandom);
+
+
+/**
+ * Execute all functions.
+ */
+function game() {
+    startGame();
+    buttonsActive();
+}
 
 /**
  * Start the Game
  */
 function startGame() {
+    let tirets = "?";
     let recupContainerMain = document.getElementById("container-main");
+    let newSpan = document.getElementById("mettreMot");
     for (let i in motRandom) {
         recupContainerMain.innerHTML += "<div id=\"" + i + "\">" + "</div>";
         recupContainerMain.style.display = "flex";
         recupContainerMain.style.justifyContent = "center";
         recupContainerMain.style.alignItems = "center";
-        document.getElementById(i).innerHTML += "<img id=grid>" + "</img>";
-        document.getElementById("grid").src = "src/images/empty.jpg";
-        document.getElementById("grid").style.borderRadius = "5px";
+        document.getElementById(i).innerHTML += "<img id=grid src='src/images/empty.jpg'/>" + "</img>";
+        newSpan.innerHTML += "<span id=\"" + i + "\">" + tirets + "</span>";
     }
 }
+
+/**
+ * Active buttons change colors
+ */
+function buttonsActive() {
+    $(document).on('click', '.buttons', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+
+    $(document).on('click', '.buttons-vitesse', function () {
+        $(this).addClass('second-active').siblings().removeClass('second-active');
+    })
+}
+
 
 /**
  * Generate a random word.
